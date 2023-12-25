0dbc1d02306ff996b4f201b50f1ee07a974379c1
Loren Bar <lorenbar@Lorens-MacBook-Pro.local>
2023-12-25 19:59:54 +0200

    my hw

--- OneOfEachStats.java
+++ OneOfEachStats.java
@@ -1,36 +1,34 @@
 import java.util.Random;
 
-public class OneOfEachStats1 {
+public class OneOfEachStats {
 
     public static void main(String[] args) {
-     boolean isAGirl=false;
-     boolean isABoy=false;
+        
 
-     int T = Integer.parseInt(args[0]);
-            int seed = Integer.parseInt(args[1]);
-            Random generator = new Random(seed);  
+        int t = Integer.parseInt(args[0]);;
+        int seed = Integer.parseInt(args[1]);;
+        Random generator = new Random(seed);
 
-
-     double sumOfNumOfChildren=0;
-     int numOfFamilyWithTwoChild=0;
-     int numOfFamilyWithThreeChild=0;
-     int numOfFamilyWithFourPlusChild=0;
-     int numOfChild=0;
-        for (int i = 0; i <a ; i++) {
+        boolean isAGirl=false;
+        boolean isABoy=false;
+        double sumOfNumOfChildren=0;
+        int numOfFamilyWithTwoChild=0;
+        int numOfFamilyWithThreeChild=0;
+        int numOfFamilyWithFourPlusChild=0;
+        int numOfChild=0;
+        for (int i = 0; i <t ; i++) {
             isAGirl=false;
             isABoy=false;
             numOfChild=0;
             while (!(isAGirl&&isABoy)){
                 if (generator.nextDouble()*1>0.5){
                     isAGirl=true;
-                    System.out.println("girl");
                 }
                 else {
                     isABoy=true;
-                    System.out.println("boy");
                 }
                 numOfChild++;
-               sumOfNumOfChildren++;
+                sumOfNumOfChildren++;
             }
             if (numOfChild==2) {
                 numOfFamilyWithTwoChild++;
@@ -39,7 +37,7 @@ public class OneOfEachStats1 {
             }else if(numOfChild>=4) {
                 numOfFamilyWithFourPlusChild++;
             }        }
-        double avg=  sumOfNumOfChildren /a;
+        double avg=  sumOfNumOfChildren /t;
         int commonNumOfChildren= Math.max(numOfFamilyWithTwoChild,Math.max(numOfFamilyWithThreeChild,numOfFamilyWithFourPlusChild));
         System.out.println("Averege "+avg+ " children to get at least one of each gender");
         System.out.println("Number of families with 2 children: "+ numOfFamilyWithTwoChild);
